b404af1dce0c0e8fa3539f2c2ac1b9d9a492fbcb
Stella2305(Cute) <dtuongvy6716@gmail.com>
2025-02-10 01:29:57 +0700

    Sign Up + Sign In (API)

--- Frontend/Bussniess-coffee-website/src/components/Header/Header.jsx
+++ Frontend/Bussniess-coffee-website/src/components/Header/Header.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import "./Header.scss";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import "./Header.scss";
 
 function Header() {
   const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
+  const user = Cookies.get("userData");
+
+  // Kiểm tra cookie khi component mount
+  useEffect(() => {
+    if (user) {
+      setUserData(JSON.parse(user)); // Chuyển JSON string thành object
+    } else {
+      setUserData(null);
+    }
+  }, [user]);
 
   const handleHome = () => navigate("/");
   const handleSignIn = () => navigate("/sign-in");
   const handleSignUp = () => navigate("/sign-up");
+
   return (
     <div className="header-container">
       <div
@@ -21,6 +36,7 @@ function Header() {
           onClick={handleHome}
         />
       </div>
+
       <div className="header-item">
         <span className="h-item">Trang chủ</span>
         <span className="h-item">Mix Cà Phê</span>
@@ -28,14 +44,30 @@ function Header() {
         <span className="h-item">Hành trình</span>
         <span className="h-item">Liên Hệ</span>
       </div>
-      <div className="header-item">
-        <button className="h-btn btn-sign-in" onClick={handleSignIn}>
-          Đăng nhập
-        </button>
-        <button className="h-btn btn-sign-up" onClick={handleSignUp}>
-          Đăng kí
-        </button>
-      </div>
+
+      {/* Kiểm tra nếu có userData trong cookie */}
+      {userData ? (
+        <div className="header-item header-user-item">
+          <FaRegUserCircle
+            size={39}
+            color="white"
+            style={{ cursor: "pointer" }}
+          />
+          <button>
+            Giỏ hàng
+            <FaCartShopping size={25} color="white" />
+          </button>
+        </div>
+      ) : (
+        <div className="header-item">
+          <button className="h-btn btn-sign-in" onClick={handleSignIn}>
+            Đăng nhập
+          </button>
+          <button className="h-btn btn-sign-up" onClick={handleSignUp}>
+            Đăng kí
+          </button>
+        </div>
+      )}
     </div>
   );
 }
